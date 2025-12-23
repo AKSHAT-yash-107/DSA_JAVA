efd5b152032b2ca1320e90d7ce05d4b246562bc2
AKSHAT-yash-107 <akshatyash107@gmail.com>
2025-12-23 22:26:31 +0530

    tree 9 commit

--- src/trees/levelorder.java
+++ src/trees/levelorder.java
@@ -3,32 +3,42 @@ import java.util.*;
     public class levelorder {
         public class TreeNode {
             int val;
-           TreeNode left;
+            TreeNode left;
             TreeNode right;
-            TreeNode() {}
-            TreeNode(int val) { this.val = val; }
+
+            TreeNode() {
+            }
+
+            TreeNode(int val) {
+                this.val = val;
+            }
+
             TreeNode(int val, TreeNode left, TreeNode right) {
                 this.val = val;
                 this.left = left;
                 this.right = right;
             }
-            public List<List<Integer>> levelOrder(TreeNode root) {
 
-                List<List<Integer>> result= new ArrayList<>();
-                if(root==null) return result;
-                Queue<TreeNode> q = new LinkedList<>();
-                q.add(root);
-                while(!q.isEmpty()){
-                    int currentlevelsize=q.size();
-                    List<Integer> levelmembers=  new ArrayList<>();
-                    for (int i = 0; i < currentlevelsize; i++) {
-                        TreeNode temp= q.poll();
-                        levelmembers.add(temp.val);
-                    if (temp.left!=null) q.add(temp.left);
-                    if (temp.right!=null) q.add(temp.right);
-                    }
-                    result.add(levelmembers);
-                }
-                return result;
+            public List<List<Integer>> levelOrder(TreeNode root) {
+                List<List<Integer>> result = new ArrayList<>();
+
+                if (root == null ) return result;
+                 Queue<TreeNode> q= new LinkedList<>();
+                 q.add(root);
+                 while(!q.isEmpty()){
+                     int size= q.size();
+                     List<Integer> level = new ArrayList<>();
+                     for (int i = 0; i < size; i++) {
+                         TreeNode temp = q.poll();
+                         level.add(temp.val);
+                         if(temp.left!= null) q.add(temp.left);
+                         if(temp.right!= null) q.add(temp.right);
+                     }
+                     result.add(level);
+                 }
+
+               return result;
             }
+        }
+    }
 
