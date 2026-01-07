332de3b09d1b000d9065361c6744913c7390d47b
AKSHAT-yash-107 <akshatyash107@gmail.com>
2026-01-07 23:42:44 +0530

    tree 14 commit

---
+++ src/strinng/palindrome.java
@@ -0,0 +1,12 @@
+package strinng;
+
+public class palindrome {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        String s = String.valueOf(x);
+        String reversed = new StringBuilder(s).reverse().toString();
+
+        return s.equals(reversed);
+    }
+}
