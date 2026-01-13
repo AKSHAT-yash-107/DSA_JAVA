99b7425b4b50509a1f60ade06c575974c749d624
AKSHAT-yash-107 <akshatyash107@gmail.com>
2026-01-13 23:41:06 +0530

    tree 22 commit

---
+++ src/trees/symmetric.java
@@ -0,0 +1,27 @@
+package trees;
+
+public class symmetric {
+      public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+    public boolean ismirror(TreeNode left,TreeNode right){
+        if(left==null && right == null) return true;
+          if (left == null || right== null ) return false;
+
+          return (left.val== right.val && ismirror(left.left,right.right) && ismirror(left.right , right.left));
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+    return ismirror(root.left,root.right);
+
+    }
+}
