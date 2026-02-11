0aae778aadd99cefcafc26532f64c59de68151af
AKSHAT-yash-107 <akshatyash107@gmail.com>
2026-02-11 22:34:53 +0530

    UPDATED COMMITTS

---
+++ src/greedy/jumpgame.java
@@ -0,0 +1,16 @@
+package greedy;
+
+public class jumpgame {
+    class Solution {
+        public boolean canJump(int[] nums) {
+            int jump=0;
+
+            for(int i =0;i <nums.length;i++){
+                if(i>jump) return false;
+                jump=Math.max(jump,nums[i]+i);
+
+            }
+            return true;
+        }
+    }
+}
