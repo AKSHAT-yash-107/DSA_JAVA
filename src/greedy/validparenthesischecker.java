4f4fd8c6a37d2495eeb790f0c4c659630a00b235
AKSHAT-yash-107 <akshatyash107@gmail.com>
2026-02-11 22:13:55 +0530

    UPDATED COMMITTS

---
+++ src/greedy/validparenthesischecker.java
@@ -0,0 +1,25 @@
+package greedy;
+
+public class validparenthesischecker {
+    public boolean checkValidString(String s){
+        int low=0,high=0;
+        for(char c : s.toCharArray()){
+            if (c==')') {
+                low++;
+                high++;
+            }
+
+            else if (c == '(') {
+                low++;
+                high--;
+            }
+            else{
+                low--;
+                high++;
+            }
+            if(high<0) return false;
+            if (low <0) low=0;
+        }
+        return low==0;
+    }
+}
