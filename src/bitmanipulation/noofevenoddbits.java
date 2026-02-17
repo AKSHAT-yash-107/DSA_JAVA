cbb7f18e98e93b49b1ada6426352da2d75a49a86
AKSHAT-yash-107 <akshatyash107@gmail.com>
2026-02-18 00:26:24 +0530

    UPDATED COMMITTS

---
+++ src/bitmanipulation/noofevenoddbits.java
@@ -0,0 +1,22 @@
+package bitmanipulation;
+
+public class noofevenoddbits {
+    class Solution {
+        public int[] evenOddBit(int n) {
+            int i =0;int even=0;int odd=0;
+            while(n>0){
+
+                if((n&1)==1){
+                    if((i%2)==0)  even++;
+                    else odd++;
+                }
+
+                n=n>>1;
+                i++;
+            }
+            return new int[]{even,odd};
+        }
+
+    }
+
+}
